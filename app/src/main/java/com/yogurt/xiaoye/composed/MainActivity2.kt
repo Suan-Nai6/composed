cee2adbe2ac22264c4eda7ecb88ab76e04399b21
Suan-Nai6 <3383787570@qq.com>
2023-07-02 21:04:55 +0800

    Changes

--- app/src/main/java/com/yogurt/xiaoye/composed/MainActivity2.kt
+++ app/src/main/java/com/yogurt/xiaoye/composed/MainActivity2.kt
@@ -3,13 +3,16 @@ package com.yogurt.xiaoye.composed
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.yogurt.xiaoye.composed.ui.theme.ComposedTheme
 import kotlin.*
@@ -24,8 +27,21 @@ class MainActivity2 : ComponentActivity() {
     }
     @Composable
     fun MessageCard(msg:Message) {
-        Text(text = msg.author)
-        Text(text = msg.body)
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null//talkback读出的内容
+            )
+            Column {
+                Text(text = msg.author)
+                Text(text = msg.body)
+            }
+        }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        MessageCard(Message("Yogurt","Xiaoye"))
     }
 }
 
